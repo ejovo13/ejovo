771df0c9e5b4973425ad06bd9f3117ed26c55d5b
Evan Voyles <ejovo13@yahoo.com>
2024-12-08 22:11:04 +0100

    Switch up the structure of my flake file

--- flake.nix
+++ flake.nix
@@ -1,8 +1,15 @@
 {
   description = "Flake wrapper for ejovo repository";
 
-  outputs = {self}: {
-    defaultPackage.x86_64-linux = import ./default.nix;
-    packages.x86_64-linux.fehWrapper = import ./default.nix;
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: {
+    defaultPackage.x86_64-linux = import ./default.nix {pkgs = nixpkgs;};
+    # packages.x86_64-linux.fehWrapper = import ./default.nix {};
   };
 }
